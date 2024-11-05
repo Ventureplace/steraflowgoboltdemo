92b9e46030e47a0658dcce1bf5cf2811cac9db3b
Iyan  Godwin <129687090+Ventureplace@users.noreply.github.com>
2024-11-05 00:14:46 -0800

    Update streamlit_app.py
    no comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Steraflow")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Thanks for coming!"
 )
